ca628744c22899e7f3dd6071eb34d5c0b06775d7
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-18 00:19:46 +0530

    roboshop infra dev

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,20 +1,21 @@
 module "bastion" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  
   ami = data.aws_ami.joindevops.id
   name = local.resource_name
-
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [local.bastion_sg_id]
-  subnet_id              = local.public_subnet_id
-  user_data = file("bastion.sh")
+  # vpc_security_group_ids = [local.bastion_sg_id]
+  # subnet_id              = local.public_subnet_id
+  vpc_security_group_ids = ["sg-09c7c70bd56f0d58b"]
+  subnet_id              = "subnet-0a3b249c1f344ef36"
   root_block_device = [{
-    encrypted  = false           # Not encrypted
-    type       = "gp3"           # gp3 volume type
-    size       = 100              # 100 GiB
-    iops       = 3000            # 3000 IOPS
-    throughput = 125             # Optional: default for gp3 is 125 MiB/s
+    encrypted             = false
+    volume_type           = "gp3"
+    volume_size           = 100
+    iops                  = 3000
+    throughput            = 125
+    delete_on_termination = true
   }]
+
   tags = merge(
     var.common_tags,
     var.bastion_tags,
